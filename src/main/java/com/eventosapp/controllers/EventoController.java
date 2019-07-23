4f406dac82964965a4ebff69bcaff1805cb60236
lucasrs97 <lucasramalho.s97@gmail.com>
2019-07-23 01:03:28 -0300

    Primeiro commit - Persistência de um Evento

---
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -0,0 +1,30 @@
+package com.eventosapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventosapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
+
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
+	public String formularioCadastro() {
+		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String formularioCadastro(Evento evento) {
+		
+		er.save(evento);
+		
+		return "redirect:/cadastrarEvento";
+	}
+	
+}
