f9e95136cf0b59fc0b2eb165f04e8fa2b8a62a2b
lucasrs97 <lucasramalho.s97@gmail.com>
2019-07-27 14:06:54 -0300

    Busca de dados no banco e alteração no layout das páginas

--- src/main/java/com/eventosapp/controllers/EventoController.java
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventosapp.models.Evento;
 import com.eventosapp.repository.EventoRepository;
@@ -25,6 +26,15 @@ public class EventoController {
 		er.save(evento);
 		
 		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> listaEventos = er.findAll();
+		mv.addObject("eventos", listaEventos);
+		return mv;
+		
 	}
 	
 }
