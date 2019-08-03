8a150523e0209debdc3645bc63d8bb6e7cccde48
lucasrs97 <lucasramalho.s97@gmail.com>
2019-08-03 18:32:34 -0300

    Finalização do projeto Spring Boot

--- src/main/java/com/eventosapp/models/Evento.java
+++ src/main/java/com/eventosapp/models/Evento.java
@@ -1,27 +1,40 @@
 package com.eventosapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-import javassist.SerialVersionUID;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Evento implements Serializable {
 	
-	private static final long SerialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long codigo;
 	
+	@NotEmpty
 	private String nome;
+	@NotEmpty
 	private String local;
+	@NotEmpty
 	private String data;
+	@NotEmpty
 	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidados;
 	
 	public long getCodigo() {
 		return codigo;
